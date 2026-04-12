c432d64ac8e22fc4ac8c2051b3a486f78f2f13c5
MasterEat <gogosblack@gmail.com>
2026-04-13 00:32:07 +0300

    Add route-based scroll reset with hash safeguard

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 
+import ScrollToTop from "@/components/ScrollToTop";
 import { appRoutes } from "@/lib/routeManifest";
 import NotFound from "@/pages/NotFound";
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      {appRoutes.map(({ path, component: Component }) => (
-        <Route key={path} path={path} element={<Component />} />
-      ))}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        {appRoutes.map(({ path, component: Component }) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
